dc08c457d18324dd11c0d2852be97b97d4ceb740
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-06 17:33:47 -0700

    nothing new

--- polls/db/seeds.rb
+++ polls/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# tony = User.new({username: 'tony'})
+# tony.save
+# 
+# tony_poll = Poll.new({title: "fish", author_id: Users.second.id })
+# tony_poll.save
+# 
+# tony_question = Question.new({question: "What are fish?", poll_id: Poll.second.id })
+# tony_question.save
+# 
+# tony_answer_choice = AnswerChoice.new({answer: "fish are food, not friends", Question.second.id})
+# tony_answer_choice.save
+# 
+# tony_response = Response.new({answer_choice_id: AnswerChoice.second.id, user_id: User.second.id})
+# tony_response.save
+
+1.upto(10) do |i|
+  #User.create!(username: "hello#{i + 10}")
+  Poll.create!({title: "fish#{i}", author_id: User.all[i].id })
+end
+p "Created 9 users!"
